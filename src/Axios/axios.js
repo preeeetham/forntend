acc4e31da35b2c3576fa04797906062a2b9cdfa7
preeetham3113 <spreethamkumar5@gmail.com>
2024-10-06 22:34:52 +0530

    .

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"https://react-production-5346.up.railway.app/api"
+    baseURL:"https://react-production-5346.up.railway.app"
 })
 export default instance
